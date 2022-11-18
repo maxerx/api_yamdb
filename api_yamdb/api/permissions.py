aadb6cadbe403f268c23d44bb260ba5de8fbb09c
Анастасия Паневская <AnastasiaPanevskaya@yandex.by>
2022-11-18 16:59:18 +0300

    permission 2

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,8 +3,5 @@ from rest_framework import permissions
 
 class IsAdminUserOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if request.user.is_authenticated:
-            return request.user.is_admin
-        return False
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.role == 'admin')

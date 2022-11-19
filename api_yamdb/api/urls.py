a47b6c343004c78fa5df1343d64ddd23b431b974
Анастасия Паневская <AnastasiaPanevskaya@yandex.by>
2022-11-19 21:12:31 +0300

    final 1

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,17 @@
+from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 from api.views import GenreViewSet, CategoryViewSet, TitleViewSet
 
 router = SimpleRouter()
 
+app_name = 'api'
 
-router.register('genre', GenreViewSet, basename='genres')
-router.register('category', CategoryViewSet, basename='categories')
-router.register('title', TitleViewSet, basename='titles')
+
+router.register('genres', GenreViewSet, basename='genres')
+router.register('categories', CategoryViewSet, basename='categories')
+router.register('titles', TitleViewSet, basename='titles')
+
+
+urlpatterns = [
+    path('v1/', include(router.urls))
+]
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,7 +21,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_filters',
     'review',
     'api',
 ]
